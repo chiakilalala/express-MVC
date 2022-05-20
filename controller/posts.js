57bf8689e33d11da099adc929886b13ecd8d1814
chiakilalala <chiaki205102@gmail.com>
2022-05-21 06:42:33 +0800

    add

--- controller/posts.js
+++ controller/posts.js
@@ -25,18 +25,19 @@ const posts ={
   
     try {
       if (user && content) {
-  
+        console.log(user,content)
           const UserExist = await Users.findById(user).exec();
           
         if (UserExist !== null ) {
-          console.log(UserExist)
+        
           const newPost = await Posts.create({
             content,  user
           });
           successhandle(res,newPost)
         
         } else {
-          errorhandle(res,"查無使用者");
+          errorhandle(res);
+          
 
         }
       }else{
